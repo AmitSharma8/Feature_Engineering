100ce36ee0370e716376d4502def1c55d618ba9f
Amit Sharma <39450934+AmitSharma8@users.noreply.github.com>
2024-12-01 08:58:08 +0530

    Update FE.py

--- FE.py
+++ FE.py
@@ -5,3 +5,12 @@ import seaborn as sns
 
 import warings 
 warnings.filterwarnings("ignore")
+
+# Download a file from Github
+url = "https://raw.githubusercontent.com/AmitSharma8/Feature_Engineering/loan.csv"
+response = requests.get(url)
+output_path = "loan"        # local file path
+with open(output_path, "wb") as file:  # Write the content to a local file
+    file.write(response.content)
+    
+data = pd.read_csv('loan')
